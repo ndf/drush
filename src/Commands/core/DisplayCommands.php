02a83b95dee9f53fb4162b6f5a7d28c3218d967c
ndf <nielsdefeyter@gmail.com>
2021-03-19 15:07:46 +0100

    Show dialog which entity_types and bundles will be rebuild before actually executing

--- src/Commands/core/DisplayCommands.php
+++ src/Commands/core/DisplayCommands.php
@@ -2,28 +2,53 @@
 
 namespace Drush\Commands\core;
 
-use Consolidation\AnnotatedCommand\CommandData;
-use Drupal\Core\Entity\EntityDisplayRepositoryInterface;
 use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\field\EntityDisplayRebuilder;
 use Drush\Commands\DrushCommands;
 use Drush\Exceptions\UserAbortException;
 
-class DisplayCommands extends DrushCommands {
+class DisplayCommands extends DrushCommands
+{
 
     /**
-     * Rebuild display configuration for all entities and bundles.
+     * Rebuild form and display configuration for all entities and bundles.
      *
-     * @command entity:rebuild-display-config
+     * @command entity:rebuild-displays
      * @bootstrap full
      * @kernel update
-     * @usage drush entity:rebuild-display-config
      *
+     * @param string $entity_type_option An entity machine name. Defaults to all.
+     * @param string $bundle_option An entity bundle name. Defaults to all.
+     *
+     * @usage drush entity:rebuild-displays
+     * @usage drush entity:rebuild-displays node
+     * @usage drush entity:rebuild-displays node article
+     * @usage drush entity:rebuild-displays all all
+     * @usage drush entity:rebuild-displays all artile
      */
-    public function entityUpdates($options = []) {
+    public function rebuildDisplays(string $entity_type_option = 'all', string $bundle_option = 'all') {
         if ($this->getConfig()->simulate()) {
             throw new \Exception(dt('rebuild-display-config command does not support --simulate option.'));
+        }
+
+        $this->output()->writeln(dt('Form and display configurations will be rebuild for the following bundles:'));
+        $this->io()->newLine();
+
+        /** @var EntityTypeBundleInfoInterface $bundle_info */
+        $bundle_info = \Drupal::service('entity_type.bundle.info')->getAllBundleInfo();
+        foreach ($bundle_info as $entity_type => $bundles) {
+            if ($entity_type !== $entity_type_option && $entity_type_option !== 'all') {
+                unset($bundle_info[$entity_type]);
+                continue;
+            }
+
+            foreach ($bundles as $bundle => $bundle_settings) {
+                if ($bundle !== $bundle_option && $bundle_option !== 'all') {
+                    unset($bundle_info[$entity_type][$bundle]);
+                    continue;
+                }
+                $this->output()->writeln("$entity_type:$bundle");
+            }
         }
 
         if (!$this->getConfig()->simulate() && !$this->io()->confirm(dt('Do you really want to continue?'))) {
@@ -32,11 +57,9 @@ class DisplayCommands extends DrushCommands {
 
         /* @var \Drupal\field\EntityDisplayRebuilder $entity_display_rebuilder */
         $entity_display_rebuilder = \Drupal::classResolver(EntityDisplayRebuilder::class);
-        $bundle_info = \Drupal::service('entity_type.bundle.info')->getAllBundleInfo();
         foreach ($bundle_info as $entity_type => $bundles) {
             foreach ($bundles as $bundle => $bundle_settings) {
-                $entity_display_rebuilder->rebuildEntityTypeDisplays($entity_type, $bundle);
-                $this->logger()->notice('Rebuild displays of: ' . $entity_type . ':' . $bundle);
+                $this->logger()->notice('Finished rebuiling form and display configurations for: ' . $entity_type . ':' . $bundle);
             }
         }
 
